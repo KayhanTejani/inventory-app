c109d1ea092c3462a80a576ae130016e0c108e1e
KayhanTejani <kayhantejani@hotmail.com>
2021-06-30 18:59:53 +0500

    setup route for products

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const productRoute = require('./routes/product');
+
 
 app.listen(3000, () => {
     console.log("Express server started at port : 3000");
 });
+
+app.use('/product', productRoute);

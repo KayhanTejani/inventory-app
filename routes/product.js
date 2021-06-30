fba88fd86d7be5cce3f04bdc95dd02010f6d2f26
KayhanTejani <kayhantejani@hotmail.com>
2021-06-30 19:52:36 +0500

    create/update product layout

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('Product route');
+    res.render("product/addOrEdit", {
+        viewTitle: "Create Product Entry"
+    });
 });
 
 module.exports = router;

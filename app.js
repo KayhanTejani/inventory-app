183480dfc8124a0033e9ecb5c167526e425ecab0
KayhanTejani <kayhantejani@hotmail.com>
2021-07-28 03:17:39 +0500

    stylesheet fixes login/signup form alignment

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
+//Load static assets
+app.use(express.static('public'));
+
 
 app.listen(3000, () => {
     console.log("Express server started at port : 3000");

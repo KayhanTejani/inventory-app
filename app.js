20a29411834e80be54326b55f2a7f563c5c8695f
KayhanTejani <kayhantejani@hotmail.com>
2021-07-29 06:18:10 +0500

    heroku port fix

--- app.js
+++ app.js
@@ -28,8 +28,12 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 
-app.listen(3000, () => {
-    console.log("Express server started at port : 3000");
+// app.listen(3000, () => {
+//     console.log("Express server started at port : 3000");
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
